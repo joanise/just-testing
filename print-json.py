8d2ce19484311ceb4b4e9558a48ab0bd6461538d
Eric Joanis <eric.joanis@gmail.com>
2025-06-24 15:13:22 -0400

    ci: make print-json.py output valid CI output format

--- print-json.py
+++ print-json.py
@@ -1,6 +1,6 @@
 import json
 
-my_dict = {"foo": {"a": 123, "b": "as'df", "c": 1.3}}
-print(json.dumps(my_dict))
-print(json.dumps(my_dict, indent=2))
+my_dict = {"foo": {"a": 123, "b": "asdf", "c": 1.3}}
+#print(json.dumps(my_dict))
+#print(json.dumps(my_dict, indent=2))
 print(f"MY_VAR='{json.dumps(my_dict)}'")
